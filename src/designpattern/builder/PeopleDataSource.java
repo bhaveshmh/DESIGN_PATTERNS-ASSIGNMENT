07af178e072826bc196fe0451f3c1bf243223db5
bhavesh.mh <bhavesh.mh@internal.media.net>
2023-08-04 19:57:47 +0530

    Complete Builder Design

--- src/designpattern/builder/PeopleDataSource.java
+++ src/designpattern/builder/PeopleDataSource.java
@@ -10,14 +10,21 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class PeopleDataSource {
+    public static String toXml(Person person) {
+        return "<Person id=\"" + person.getId() + "\" name=\"" + person.getName() + "\">" +
+                "<Address><City>" + person.getCity() + "</City><Country>" + person.getCountry()
+                + "</Country></Address>" +
+                "</Person>";
+    }
+
     public static String getPeopleXml(List<Person> persons) {
         String finalXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
         finalXML += "<People number=\"" + persons.size() + "\">";
+
         for (Person person : persons) {
-            finalXML += "<Person id=\"" + person.getId() + "\" name=\"" + person.getName() + "\">" +
-                    "<Address><City>" + person.getCity() + "</City><Country>" + person.getCountry() + "</Country></Address>" +
-                    "</Person>";
+            finalXML += toXml(person);
         }
+
         finalXML += "</People>";
         return finalXML;
     }
--- src/designpattern/builder/Person.java
+++ src/designpattern/builder/Person.java
@@ -7,6 +7,39 @@ package designpattern.builder;
  * Time: 11:31:11 PM
  * To change this template use File | Settings | File Templates.
  */
+
+class PersonBuilder {
+    private String name;
+    private int id;
+    private String city;
+    private String country;
+
+    public PersonBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public PersonBuilder city(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public PersonBuilder country(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public Person build() {
+        return new Person(name, id, city, country);
+    }
+
+}
+
 public class Person {
     private String name;
     private int id;
